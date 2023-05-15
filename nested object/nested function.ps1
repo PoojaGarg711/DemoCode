7a373c1fa6d24a5a6a946feb2058c025cf276184
pooja garg <pooja.garg@atimetals.onmicrosoft.com.onmicrosoft.com>
2023-05-15 13:28:44 +0530

    nested object challenge 3

---
+++ nested object/nested function.ps1
@@ -0,0 +1,27 @@
+﻿const searchTerm = "britania"
+let result = []
+
+function getEachItem {
+    param([object]$object )
+  object.forEach(item => {
+    searchItem(item)
+  })
+  let uniqueResults = [...new Set(result)]
+  return uniqueResults
+}
+
+function searchItem {
+    param([object]$item)
+
+  Object.keys($item).forEach($key => {
+    if (typeof $item[$key] -eq 'object') {
+      searchItem($item[$key])
+    }
+    if (typeof $item[$key] -eq 'string') {
+      let searchAsRegEx = new RegExp($searchTerm, "gi");
+      if (item[key].match(searchAsRegEx)) {
+        result.push(item.id)
+      }
+    }
+  })
+}
